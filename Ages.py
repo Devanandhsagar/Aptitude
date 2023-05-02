e750dce047e3001d53b150cc216d5ea1c1d8bf5f
Devaanandhsagar <madamshatidevanandh@gmail.com>
2023-05-02 16:51:18 +0530

    sum of ages and persent and past age

--- Ages.py
+++ Ages.py
@@ -61,19 +61,55 @@ elif (given_data== "Difference"):
     person_1_present_age = (person_1) * (value_of_x)
     person_2_present_age = (person_2) * (value_of_x)
     print("Difference of two persons : "+str((person_1_present_age)-(person_2_present_age)))
-elif (given_data=="sum of persons given"):
+elif (given_data=="sum of persons given present ages"):
     person_1 = int(input("enter the age ratio : "))
     person_2 = int(input("enter the age ratio : "))
     year = int(input("enter year : "))
-    year_before_or_after=input("if year is hence enter the 1 else enter the 0 or enter -1 for ago")
-    sum_ages = int(input("enter the sum"))
+    year_before_or_after=input("if year is hence or after enter the 1 else enter the 0 for both persent ages or enter -1 for ago : ")
+    sum_ages = int(input("enter the sum : "))
     value_of_x=sum_ages/(person_1 + person_2)
     if (year_before_or_after== "1" ):
         print(((person_1*value_of_x))+year)
     elif (year_before_or_after=="0"):
         print(person_1 * value_of_x)
+        print(person_2 * value_of_x)
     else :
         print(((person_1 * value_of_x)) - year)
+elif (given_data=="sum of persons given after ages"):
+    person_1 = int(input("enter the times of persons_2 : "))
+    person_2 = int(input("enter the times nothing enter 1 : "))
+    year = int(input("enter year : "))
+    year_before_or_after=input("if year is hence or after enter the 1 else enter the 0 for both persent ages or enter -1 for ago : ")
+    sum_ages = int(input("enter the sum : "))
+    upper=abs((sum_ages+year)-(person_1 * year))
+    lower=abs(person_2+(person_1*(person_2)))
+    value_of_x=upper/lower
+    if (year_before_or_after== "1" ):
+        print(person_1*value_of_x)
+    elif (year_before_or_after=="0"):
+        print(sum_ages-(person_2*value_of_x))
+        print(person_2 * value_of_x)
+    else :
+        print(((person_1 * value_of_x)))
+
+elif (given_data=="sum of three persons and ages ratio"):
+    person_1 = int(input("enter the age ratio of persons_1 : "))
+    person_2 = int(input("enter the age ratio of persons_2 : "))
+    person_3=int(input("enter the age ratio of person_3 : "))
+    year = int(input("enter year : "))
+    persent_ages_of_persons= input(
+        "if person_1 agw enter 1 or person_2 age enter 2 or person_3 age enter 3 : ")
+    sum_ages = int(input("enter the sum : "))
+    upper = abs(sum_ages - (3*year))
+    lower = abs(person_1 + person_1 +person_2)
+    value_of_x = upper / lower
+    if (persent_ages_of_persons=="1"):
+        print(person_1*value_of_x)
+    elif(persent_ages_of_persons=="2"):
+        print(person_2*value_of_x)
+    else:
+        print(person_3*value_of_x)
+
 
 
 
