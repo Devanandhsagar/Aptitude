622e74185d4ac3d79aa46b839a01b87b9a2eb231
Devaanandhsagar <madamshatidevanandh@gmail.com>
2023-05-03 22:37:53 +0530

    problems on age

---
+++ test questions.py
@@ -0,0 +1,9 @@
+## question 17
+a=int(input("enter value for a : "))
+b=int(input("enter value for b : "))
+c=int(input("enter the hence year : "))
+d=int(input("enter the ratio_1 : "))
+e=int(input("enter the ratio_2 : "))
+f=((d)*(c)-(e)*(c))/((e)*(a)-(d)*(b))
+formula=(a-b)*f
+print(formula)
